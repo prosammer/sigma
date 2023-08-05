b8614e93624f12e79ae5e7fa4e2541ea40e69b5c
prosammer <sam.finton@gmail.com>
2023-08-04 19:19:38 -0700

    Working quick start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import Greet from "$lib/Greet.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Greet />
