b43b1506256828cd765e0dc9c278b5a2cad05c6e
prosammer <sam.finton@gmail.com>
2023-08-04 19:34:44 -0700

    Added dotenv to rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenv::dotenv;
+use std::env;
+
+
 fn main() {
+  dotenv().ok();
+
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
@@ -10,5 +16,7 @@ fn main() {
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    let openai_api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
+
+   format!("Hello, {}!", openai_api_key)
 }
