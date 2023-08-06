83ac77ba4064556731d0df9382aed9b4898064e9
prosammer <sam.finton@gmail.com>
2023-08-05 20:42:56 -0700

    Switched to using RecordRTC

--- src/lib/VideoWindow.svelte
+++ src/lib/VideoWindow.svelte
@@ -1,75 +1,45 @@
 <script>
-    import { onMount, onDestroy } from 'svelte';
-    import 'video.js/dist/video-js.min.css';
-    import 'videojs-record/dist/css/videojs.record.css';
+    import { onMount } from "svelte";
+    import {dialog, fs} from "@tauri-apps/api";
 
     let videoElement;
-    let videojs = undefined;
-    let player = undefined;
-    let RecordRTC = undefined;
-    let adapter = undefined;
-    let Record = undefined; // New Line
+    let stream;
+    let recorder;
+    let downloadURL;
 
 
     onMount(async () => {
-        const videojsImport = import('video.js');
-        const recordImport = import('videojs-record/dist/videojs.record.js'); // New Line
-        const recordrtcImport = import('recordrtc');
-        const adapterImport = import('webrtc-adapter');
-        [videojs, Record, RecordRTC, adapter] = await Promise.all([videojsImport, recordImport, recordrtcImport, adapterImport]); // Updated Line
-
-
-        // options for the player
-        let options = {
-            controls: true,
-            bigPlayButton: false,
-            width: 320,
-            height: 240,
-            fluid: false,
-            plugins: {
-                record: {
-                    audio: true,
-                    video: true,
-                    maxLength: 10,
-                    debug: true
-                }
-            }
-        };
-
-        // initialize video.js
-        player = videojs.default(videoElement, options, function() {
-            var msg = 'Using video.js ' + videojs.default.VERSION +
-                ' with videojs-record ' + videojs.default.getPluginVersion('record') +
-                ' and recordrtc ' + RecordRTC.version;
-            videojs.default.log(msg);
-        });
-
-        // error handling
-        player.on('deviceError', function() {
-            console.warn('device error:', player.deviceErrorCode);
-        });
-
-        player.on('error', function(element, error) {
-            console.error("ERROR!!");
-            console.error(error);
-        });
-
-        // user clicked the record button and started recording
-        player.on('startRecord', function() {
-            console.log('started recording!');
-        });
-
-        // user completed recording and stream is available
-        player.on('finishRecord', function() {
-            console.log('finished recording: ', player.recordedData);
-        });
+        const { default: RecordRTC } = await import('recordrtc');
+        startCamera();
     });
 
-    onDestroy(() => {
-        if (player) {
-            player.dispose();
+    async function startCamera() {
+        try {
+            stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            videoElement.srcObject = stream;
+            videoElement.play();
+        } catch (error) {
+            console.error("Error accessing the camera:", error);
         }
-    });
+    }
+
+    async function startRecording() {
+        const { default: RecordRTC } = await import('recordrtc');
+
+        recorder = RecordRTC(stream, {
+            type: 'video'
+        });
+        recorder.startRecording();
+    }
+
+    async function stopRecording() {
+        recorder.stopRecording(async function () {
+            let blob = recorder.getBlob();
+            // save the file
+        });
+    }
 </script>
 
-<video id="myVideo" playsinline class="video-js vjs-default-skin" bind:this={videoElement}></video>
+<video bind:this={videoElement} width="640" height="480" autoplay playsinline></video>
+<button on:click={startRecording}>Start Recording</button>
+<button on:click={stopRecording}>Stop and Save</button>
