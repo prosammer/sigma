42c39016056f57ad6580cd71916e63c7c756c881
Prosammer <sam.finton@gmail.com>
2023-08-11 13:18:26 -0700

    Added shadcdn-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,17 @@ const config = {
 	optimizeDeps: {
 		include: ['video.js', 'videojs-record']
 	},
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: "src/lib/components",
+			"$components/*": "src/lib/components/*"
+		}
 	}
 };
 
