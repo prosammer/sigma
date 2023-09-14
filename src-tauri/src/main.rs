bf224c86c6f87722f731c93edc2e843f6343544f
Prosammer <sam.finton@gmail.com>
2023-09-14 09:22:07 -0300

    Added is_production env var - in dev, voice chat starts immediately

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,14 +5,13 @@ mod text_to_speech;
 mod utils;
 
 use dotenv::dotenv;
-use std::{thread, time::Duration};
+use std::{env, thread, time::Duration};
 use std::path::PathBuf;
 use std::sync::mpsc;
 use std::thread::sleep;
 
 use tauri::{SystemTray, CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, Manager, WindowUrl, WindowBuilder, ActivationPolicy};
 use chrono::{Local, NaiveTime, Timelike};
-use tauri_plugin_positioner::{Position, WindowExt};
 use tauri_plugin_autostart::MacosLauncher;
 use tauri::Wry;
 use tauri_plugin_store::with_store;
@@ -25,7 +24,7 @@ use crate::whisper_command::run_transcription;
 fn main() {
     dotenv().ok();
 
-    let record = CustomMenuItem::new("record".to_string(), "Record");
+    let record = CustomMenuItem::new("talk".to_string(), "Talk");
     let settings = CustomMenuItem::new("settings".to_string(), "Settings");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let tray_menu = SystemTrayMenu::new()
@@ -38,6 +37,7 @@ fn main() {
 
     let mut app = tauri::Builder::default()
         .setup(|app| {
+            let is_production = env::var("IS_PRODUCTION").map_or(false, |v| v == "true");
             let home_dir = dirs::home_dir().expect("Failed to get home directory");
             let path = home_dir.join("Movies/Video Journals/");
 
@@ -45,7 +45,14 @@ fn main() {
                 std::fs::create_dir_all(&path).expect("Failed to create directory");
             }
 
-            start_notification_loop(app.handle());
+
+            // If we're in production, start waiting for the right time to start the voice chat. In dev, start it right away.
+            if is_production {
+                start_notification_loop(app.handle());
+            } else {
+                start_voice_chat();
+            }
+
             Ok(())
         })
         .plugin(tauri_plugin_positioner::init())
@@ -57,42 +64,15 @@ fn main() {
             match event {
                 tauri::SystemTrayEvent::MenuItemClick { id, .. } => {
                     match id.as_str() {
-                        "record" => {
-                            // let window_exists = app.get_window("recording_window").is_some();
-                            // if !window_exists {
-                            //     let _window = create_recording_window(&app);
-                            // }
-                            let (transcription_tx, transcription_rx) = mpsc::channel();
-                            let (talking_tx, talking_rx) = mpsc::channel();
-
-                            thread::spawn(move || {
-                                run_transcription(transcription_tx, talking_rx).unwrap();
-                            });
-
-                            thread::spawn(move || {
-                                let runtime = Runtime::new().unwrap();
-
-                                runtime.block_on(async {
-                                    loop {
-                                        sleep(Duration::from_millis(200));
-                                        if let Ok(transcribed_words) = transcription_rx.recv() {
-                                            println!("Transcribed words: {}", transcribed_words);
-                                            let gpt_response = get_completion(transcribed_words).await.expect("Unable to get completion");
-                                            println!("GPT Response: {}", gpt_response);
-                                            let speech_audio = text_to_speech("2EiwWnXFnvU5JabPnv8n",gpt_response).await.expect("Unable to run TTS");
-                                            play_audio(speech_audio);
-                                            talking_tx.send(false);
-                                        }
-                                    }
-                                });
-                            });
+                        "talk" => {
+                            start_voice_chat();
                         }
                         "settings" => {
                             let window_exists = app.get_window("settings_window").is_some();
                             if !window_exists {
                                 let _window = create_settings_window(&app);
                             }
-                        }e
+                        }
                         _ => {}
                     }
                 }
@@ -113,11 +93,36 @@ fn main() {
 }
 
 
+fn start_voice_chat() {
+    let (transcription_tx, transcription_rx) = mpsc::channel();
+    let (talking_tx, talking_rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        run_transcription(transcription_tx, talking_rx).unwrap();
+    });
+
+    thread::spawn(move || {
+        let runtime = Runtime::new().unwrap();
+
+        runtime.block_on(async {
+            loop {
+                if let Ok(transcribed_words) = transcription_rx.recv() {
+                    println!("Transcribed words: {}", transcribed_words);
+                    let gpt_response = get_completion(transcribed_words).await.expect("Unable to get completion");
+                    println!("GPT Response: {}", gpt_response);
+                    let speech_audio = text_to_speech("2EiwWnXFnvU5JabPnv8n",gpt_response).await.expect("Unable to run TTS");
+                    play_audio(speech_audio);
+                    talking_tx.send(false);
+                }
+            }
+        });
+    });
+}
 
 fn start_notification_loop(handle: tauri::AppHandle) {
     thread::spawn(move || {
         loop {
-            thread::sleep(Duration::from_secs(59));
+            sleep(Duration::from_secs(59));
 
             let stores = handle.state::<StoreCollection<Wry>>();
             let path = PathBuf::from(".settings.dat");
@@ -139,31 +144,11 @@ fn start_notification_loop(handle: tauri::AppHandle) {
 
 
             if now.hour() == parsed_time.unwrap().hour() && now.minute() == parsed_time.unwrap().minute() {
-                println!("Opening recording window");
-                let window_exists = handle.get_window("recording_window").is_some();
-                if !window_exists {
-                    let _window = create_recording_window(&handle);
-                }
+                println!("Chosen time reached! Starting voice chat");
+                start_voice_chat();
             }
         }
     });
-}
-
-fn create_recording_window(handle: &tauri::AppHandle) -> tauri::Window {
-    let new_window = WindowBuilder::new(
-        handle,
-        "recording_window",
-        WindowUrl::App("recording".into())
-    )
-        .decorations(false)
-        .transparent(true)
-        .always_on_top(true)
-        .inner_size(192.0,192.0)
-        .build()
-        .expect("Failed to create recording_window");
-
-    new_window.move_window(Position::TopCenter).expect("Failed to center window");
-    new_window
 }
 
 fn create_settings_window(handle: &tauri::AppHandle) -> tauri::Window {
