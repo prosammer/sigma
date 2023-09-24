8cf3055598b2a3700641bcbeb3260fae20beb748
Prosammer <sam.finton@gmail.com>
2023-09-24 13:47:01 -0300

    Added a leave function

--- src-tauri/src/text_to_speech.rs
+++ src-tauri/src/text_to_speech.rs
@@ -2,22 +2,40 @@ use std::collections::HashMap;
 use std::env;
 use anyhow::{Error, Result};
 use async_openai::Client;
-use async_openai::types::{ChatCompletionRequestMessage, CreateChatCompletionRequestArgs, Role};
+use async_openai::types::{ChatCompletionFunctionsArgs, ChatCompletionRequestMessage, CreateChatCompletionRequestArgs, Role};
 use bytes::Bytes;
+use serde_json::json;
 use crate::whisper::create_chat_completion_request_msg;
 
 pub async fn get_gpt_response(messages: Vec<ChatCompletionRequestMessage>) -> Result<ChatCompletionRequestMessage, Error> {
     let client = Client::new();
 
+    let function = ChatCompletionFunctionsArgs::default()
+        .name("leave_conversation")
+        .description("The GPT AI can choose to call this function to leave the conversation whenever it appears finished, or if the user is unintelligible more than 3 times in a row.")
+        .parameters(json!({"type": "object", "properties": {}}))
+        .build()?;
+
     let request = CreateChatCompletionRequestArgs::default()
         .model("gpt-3.5-turbo")
         .max_tokens(120_u16)
         .messages(messages.clone())
+        .functions(vec![function])
         .build()?;
 
     let resp = client.chat().create(request).await?;
 
-    let bot_string = resp.choices[0].message.content.as_ref().unwrap().clone();
+    let resp_message = resp.choices.get(0).unwrap().message.clone();
+
+    if let Some(function_call) = resp_message.function_call {
+        if function_call.name == "leave_conversation" {
+            return Ok(create_chat_completion_request_msg(
+                "Goodbye!".to_string(),
+                Role::System));
+        }
+    }
+
+    let bot_string = resp_message.content.as_ref().unwrap().clone();
 
     let new_bot_message = create_chat_completion_request_msg(
         bot_string,
